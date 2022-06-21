45e018fd02874e3cb8f26771b7e1b262106f4b4a
Md. Ishtiak Ahmed <42693745+ishtiakrongon@users.noreply.github.com>
2022-06-21 16:31:42 +0600

    Update modelling_distributions_2_1.py

--- modelling_distributions_2_1.py
+++ modelling_distributions_2_1.py
@@ -8,15 +8,42 @@
 
 # Importing Libraries
 
+from unicodedata import normalize
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-
+from scipy.stats import norm
+from empiricaldist import Cdf
 #-------------------------------------------------------------------------------
 
 
+# THE NORMAL DISTRIBUTION
+
+sample = np.random.normal( size = 1000 )
+
+Cdf(sample).plot()
+
+plt.show()
 
 
 
+#-------------------------------------------------------------------------------
+
+# The Normal CDF
+
+xs = np.linspace( -3, 3 )       # creating equally spaced array
+ys = norm(0, 1).cdf(xs)         # here norm(0, 1) creates an object that
+                                # represents a normal distribution with mean 0
+                                # and standard deviation 1.
+
+#-------------------------------------------------------------------------------
+
+# Plotting the results
+
+plt.plot(xs, ys , color = 'gray')
+Cdf(sample).plot()
+
+plt.show()
 
 
+#-------------------------------------------------------------------------------
