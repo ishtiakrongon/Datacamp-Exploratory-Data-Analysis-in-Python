5b9bc2d97e314dc1adfee80b05e9cbbd4ca9491d
Md. Ishtiak Ahmed <42693745+ishtiakrongon@users.noreply.github.com>
2022-06-21 16:06:08 +0600

    Added new python file
    This file contains some lectures of Distributions notebook.

---
+++ modelling_distributions_2_1.py
@@ -0,0 +1,22 @@
+# Modeling Distributions
+# This is the part of the Distributions notebook
+
+
+#-------------------------------------------------------------------------------
+
+# THE NORMAL DISTRIBUTION (Also known as Gaussian Distribution)
+
+# Importing Libraries
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+#-------------------------------------------------------------------------------
+
+
+
+
+
+
+
